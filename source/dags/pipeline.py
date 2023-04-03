598179a3be7c8bdc9619887b7a2d58679f63d09e
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2023-04-03 23:51:15 +0000

    add new operator and fix dependencies
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- source/dags/pipeline.py
+++ source/dags/pipeline.py
@@ -2,7 +2,7 @@ from airflow.models import DAG
 from airflow.contrib.file_sensor import FileSensor
 from airflow.operators.python import PythonOperator
 from datetime import date, datetime
-from source.scripts import extract_table_to_csv
+from source.scripts import extract_table_to_csv, ingest_csv_to_pyspark
 
 dag = DAG(dag_id='etl_flow', default_args={'start_date': datetime(2023, 4, 10)})
 
@@ -12,10 +12,16 @@ sensor = FileSensor(task_id='sense_csv_files',
                     timeout=15,
                     dage=dag)
 
-python_task = PythonOperator(
+extraction_task = PythonOperator(
     task_id="extract_table_to_csv",
     python_callable=extract_table_to_csv.write_table_data_to_csv,
     dag=dag
 )
 
-sensor >> python_task
+ingestion_task = PythonOperator(
+    task_id="ingest_csv_to_pyspark",
+    python_callable=ingest_csv_to_pyspark.ingest_into_spark_df,
+    dag=dag
+)
+
+extraction_task >> sensor >> ingestion_task
