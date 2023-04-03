c560d31506944689f9140ea7b40eeaa21aa30652
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2023-04-03 08:29:38 +0000

    create config.py file
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

---
+++ source/config.py
@@ -0,0 +1,6 @@
+DATABASE_SQLAlchemy_URL_CONFIG = {
+    "PostgreSQL": "postgresql+psycopg2://",
+    "MySQL": "mysql+pymysql://",
+    "MariaDB": "mysql+pymysql://",
+    "MS-SQL-Server": "mssql+pyodbc://"
+}
