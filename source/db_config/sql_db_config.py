f83382e40c8adeb37892a8106b85caf672f1f87e
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2023-04-19 21:52:30 +0000

    add files for neo4j and mongo database configuration
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- source/db_config/sql_db_config.py
+++ source/db_config/sql_db_config.py
@@ -1,16 +1,5 @@
 import mysql.connector
 from mysql.connector import errorcode
-import os
-from dotenv import load_dotenv
-
-# loading the environment variables
-load_dotenv()
-
-SERVER = os.environ.get("DB_SERVER")
-PORT = int(os.environ.get("DB_PORT"))
-USER = os.environ.get("DB_USER")
-DB_NAME = os.environ.get("DATABASE")
-PSSWD = os.environ.get("DB_PSSWD")
 
 
 def connect_to_db(server: str, port: int, user: str, db_name: str, psswd: str):
@@ -18,12 +7,13 @@ def connect_to_db(server: str, port: int, user: str, db_name: str, psswd: str):
         'host': server,
         'user': user,
         'password': psswd,
-        'database': db_name
+        'database': db_name,
+        'port': port
     }
     # construct connection string
     try:
         conn = mysql.connector.connect(**config)
-        print("Connection established")  # change to log
+        print("Connection established")  # change to logging
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with the user name or password")
@@ -34,4 +24,6 @@ def connect_to_db(server: str, port: int, user: str, db_name: str, psswd: str):
     else:
         cursor = conn.cursor()
 
-    return cursor
+        return cursor
+
+    return None
