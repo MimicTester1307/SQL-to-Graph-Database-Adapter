7aa4e2de267bc66a2e85deb53f654df0293b31b4
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2023-04-20 12:27:36 +0000

    check if specified location exists
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- src/scripts/ingest_csv_to_pyspark.py
+++ src/scripts/ingest_csv_to_pyspark.py
@@ -14,15 +14,18 @@ def ingest_into_spark_df(spark_session, csv_path):
     stored_dfs = {}
 
     # loop through the list of CSV files and load them into PySpark one at a time
-    for file in os.listdir(csv_path):
-        # load the CSV into a DataFrame
-        df = spark_session.read.csv(f'{csv_path}{file}', header=True, inferSchema=True)
+    if os.path.exists(csv_path):
+        for file in os.listdir(csv_path):
+            # load the CSV into a DataFrame
+            df = spark_session.read.csv(f'{csv_path}{file}', header=True, inferSchema=True)
 
-        # split the file name to get the first part. Will be used during transformation
-        split_file_name = file.split('.')
+            # split the file name to get the first part. Will be used during transformation
+            split_file_name = file.split('.')
 
-        # store df and corresponding name in dict
-        stored_dfs[split_file_name[0]] = df
+            # store df and corresponding name in dict
+            stored_dfs[split_file_name[0]] = df
+    else:
+        print("The specified location does not exist")
 
     return stored_dfs
 
