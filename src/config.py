5f90d231a4a3be6f33f87fa26ccb518984b3adb5
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2023-04-20 04:25:25 +0000

    delete config.py file
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- src/config.py
+++
@@ -1,6 +0,0 @@
-DATABASE_SQLAlchemy_URL_CONFIG = {
-    "PostgreSQL": "postgresql+psycopg2://",
-    "MySQL": "mysql+pymysql://",
-    "MariaDB": "mysql+pymysql://",
-    "MS-SQL-Server": "mssql+pyodbc://"
-}
