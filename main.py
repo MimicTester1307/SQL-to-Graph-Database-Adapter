f9431596f105035299755c8b47751b93dae41026
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2023-04-22 21:41:37 +0000

    main file
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from pyspark.sql import SparkSession
 from src.db_config.sql_db_config import connect_to_db
 from src.scripts.extract_table_to_csv import write_table_data_to_csv
 from src.scripts.ingest_csv_to_pyspark import ingest_into_spark_df
-from src.transformations.transform_df_to_node import transform_df_to_node
-from src.db_config.neo4j_aura_db_config import App
+from src.transformations.transform_df_to_node_spark import transform_df_to_node
 
 # connect to the database
 load_dotenv()    # load environment variables
@@ -19,34 +18,42 @@ DB_NAME = os.environ.get("DATABASE")
 PSSWD = os.environ.get("DB_PSSWD")
 SSL_CA = os.environ.get("SSL_CA")
 
-conn = connect_to_db(server=SERVER, port=PORT, user=USER, psswd=PSSWD, db_name=DB_NAME, ssl_ca_path=SSL_CA)
-if conn is None:
-    print("Invalid connection object.")   # add log; don't proceed further
-# if connection is None, don't proceed further
 
-table_names = ['products', 'orders', 'order_details']
+try:
 
-# begin extraction process
-write_table_data_to_csv(output_location='src/out/', table_list=table_names, batch_size=500, conn=conn)
-conn.close()
+    conn = connect_to_db(server=SERVER, port=PORT, user=USER, psswd=PSSWD, db_name=DB_NAME, ssl_ca_path=SSL_CA)
+    if conn is None:
+        print("Invalid connection object.")   # add log; don't proceed further
+        # if connection is None, don't proceed further
 
-# Begins CSV to PySpark Ingestion process
-spark = SparkSession.builder.appName("Ingest Multiple CSV to PySpark").getOrCreate()
-csv_folder_path = "src/out/"
-df_dict = ingest_into_spark_df(spark_session=spark, csv_path=csv_folder_path)
+    table_names = ['products', 'orders', 'order_details']
 
+    # begin extraction process
+    write_table_data_to_csv(output_location='src/out/', table_list=table_names, batch_size=500, conn=conn)
 
-# Loading Pandas DataFrames into Neo4j
-# Neo4j Database Configurations
-BOLT_URI = os.environ.get("NEO4J_URI")
-NEO4J_USER = os.environ.get("NEO4J_USRNAME")
-NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWD")
+    # Begins CSV to PySpark Ingestion process
+    spark = SparkSession.builder.appName("Ingest Multiple CSV to PySpark")\
+        .config('spark.jars.packages', 'neo4j-contrib:neo4j-connector-apache-spark_2.12:4.0.1_for_spark_3')\
+        .getOrCreate()
+    csv_folder_path = "src/out/"
+    df_dict = ingest_into_spark_df(spark_session=spark, csv_path=csv_folder_path)
 
-app = App(uri=BOLT_URI, user=NEO4J_USER, password=NEO4J_PASSWORD)  # graph db instance
+    # Loading Pandas DataFrames into Neo4j
+    # Neo4j Database Configurations
+    BOLT_URI = os.environ.get("NEO4J_URI")
+    NEO4J_USER = os.environ.get("NEO4J_USRNAME")
+    NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWD")
 
-# Begin transformation to nodes
-transform_df_to_node(neo4j_instance_object=app, df_dict=df_dict)
+    config = {
+        'uri': BOLT_URI,
+        'password': NEO4J_PASSWORD,
+        'user': NEO4J_USER
+    }
+    # Begin transformation to nodes
+    transform_df_to_node(df_dict=df_dict, config_dict=config)
 
+finally:
+    conn.close()
+    # app.driver.close()
+    spark.stop()
 
-# Stop Spark session
-spark.stop()
